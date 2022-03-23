ed1966b8fd71e02666805aedb77e2f1790842b56
ShrutiBoparti <sruthi.b@navtech.io>
2022-03-23 12:53:28 +0530

    add console application

---
+++ DotNETTraining/DotNETTraining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNETTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
